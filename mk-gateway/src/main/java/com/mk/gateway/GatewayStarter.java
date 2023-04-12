b50d066c5e0efb8798a826741e7b6b31efe611cb
wangchen <516760573@qq.com>
2023-04-12 19:21:32 +0800

    mk-gateway模块

--- mk-gateway/src/main/java/com/mk/gateway/GatewayStarter.java
+++ mk-gateway/src/main/java/com/mk/gateway/GatewayStarter.java
@@ -3,6 +3,9 @@ package com.mk.gateway;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Predicate;
 
 /**
  * Hello world!
@@ -12,5 +15,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GatewayStarter {
     public static void main(String[] args) {
         SpringApplication.run(GatewayStarter.class, args);
+    }
+
+    /**
+     * 用于readBody断言，可配置到yml
+     */
+    @Bean
+    public Predicate<Boolean> bodyPredicate(){
+        return new Predicate() {
+            @Override
+            public boolean test(Object o) {
+                log.info("o:{}", o);
+                return true;
+            }
+        };
     }
 }
