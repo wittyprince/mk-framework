defa00a0e90ecd0b216650f93590a5c791e33094
wangchen <516760573@qq.com>
2023-04-12 17:17:03 +0800

    gateway模块

---
+++ mk-gateway/src/main/java/com/mk/gateway/filter/GatewayFilterFactory.java
@@ -0,0 +1,16 @@
+package com.mk.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * GatewayFilterFactory
+ *
+ * @author WangChen
+ * Created on 2023/4/12
+ * @since 1.0
+ */
+@Slf4j
+@Component
+public class GatewayFilterFactory {
+}
---
+++ mk-gateway/src/main/java/com/mk/gateway/predicate/CustomReadBodyRoutePredicateFactory.java
@@ -0,0 +1,11 @@
+package com.mk.gateway.predicate;
+
+/**
+ * CustomReadBodyRoutePredicateFactory
+ *
+ * @author WangChen
+ * Created on 2023/4/12
+ * @since 1.0
+ */
+public class CustomReadBodyRoutePredicateFactory {
+}
